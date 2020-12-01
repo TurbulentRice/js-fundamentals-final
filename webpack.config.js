e88653aa8b9eb0cb7c4253105f0829a5b9217ff7
Sean Russell <seanrussellphotography@gmail.com>
2020-11-30 22:16:36 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // Your input file
+  output: {
+    filename: "main.js", // Your output filename
+    path: path.resolve(__dirname, "./public/assets"), // Output file path
+    publicPath: "/assets/", // Folder where all Webpack generated code will go
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+
+  devServer: {
+    port: 7000,
+    publicPath: "/assets/", // Folder where all Webpack generated code will go
+    contentBase: path.resolve(__dirname, "./public"), // Folder that has your index.html file
+    watchContentBase: true // Makes it so the browser will refresh when you make changes to the index.html file too
+  }
+};
