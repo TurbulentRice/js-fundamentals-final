9056a9c578ce96d3d7f292155366c6bd41880e44
Sean Russell <seanrussellphotography@gmail.com>
2021-09-23 00:15:02 -0400

    updated API, env

--- src/async.js
+++ src/async.js
@@ -1,20 +1,25 @@
 const axios = require("axios").default;
 
+const api = {
+  HOST: process.env.API_HOST,
+  KEY: process.env.API_KEY,
+  BASE_URL: `https://${process.env.API_HOST}`
+}
+
 // Requests
 
 const apiOptions = (url) => ({
   method: "GET",
   url,
   headers: {
-    'x-rapidapi-key': process.env.API_KEY,
-    'x-rapidapi-host': "yahoo-finance15.p.rapidapi.com"
+    'x-rapidapi-key': api.KEY,
+    'x-rapidapi-host': api.HOST
   }
 })
 
 export const getMostWatched = () => {
-  const url = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/tr/trending"
-  const options = apiOptions(url)
-  return axios.request(options)
+  const url = api.BASE_URL + "/tr/trending"
+  return axios.request(apiOptions(url))
     .then(response => response.data)
     .then(data => data[0].quotes.slice(0, 10))
     .then(symbols => getQuote(symbols))
@@ -22,9 +27,10 @@ export const getMostWatched = () => {
 }
 
 export const getTopGainers = () => {
-  const url = "https://yahoo-finance15.p.rapidapi.com/api/yahoo/ga/topgainers"
-  const options = apiOptions(url)
-  return axios.request(options)
+  // Removed 9/23, changed endpoints
+  // const url = api.BASE_URL + "/ga/topgainers"
+  const url = api.BASE_URL + "/co/collections/day_gainers"
+  return axios.request(apiOptions(url))
     .then(response => response.data.quotes)
     .catch(error => console.log(error))
 }
@@ -32,29 +38,25 @@ export const getTopGainers = () => {
 export const getQuote = (symbol) => {
   let isMultiple = Array.isArray(symbol);
   let search = isMultiple ? symbol.join(",") : symbol.trim()
-  const url = `https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/${search}`
-  const options = apiOptions(url)
+  const url = api.BASE_URL + `/qu/quote?symbol=${search}`
 
-  return axios.request(options)
+  return axios.request(apiOptions(url))
     .then(response => isMultiple ? response.data : response.data[0])
     .catch(error => console.log(error))
 }
 
 export const getProfile = (symbol) => {
-  const url = `https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/${symbol}/asset-profile`
-  const options = apiOptions(url)
+  const url = api.BASE_URL + `/qu/quote/${symbol}/asset-profile`
 
-  return axios.request(options)
+  return axios.request(apiOptions(url))
     .then(response => response.data.assetProfile)
     .catch(error => console.log(error))
 }
 
 export const getStockHistory = (symbol, interval) => {
-  const url  =`https://yahoo-finance15.p.rapidapi.com/api/yahoo/hi/history/${symbol}/${interval}`
-  const options = apiOptions(url)
+  const url  = api.BASE_URL + `/hi/history/${symbol}/${interval}`
 
-
-  return axios.request(options)
+  return axios.request(apiOptions(url))
     .then(response => response.data)
     .catch(error => console.log(error))
 }
